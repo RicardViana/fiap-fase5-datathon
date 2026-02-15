de9414958c1c19f21fa48af25a815e47cbc0f756
Ricardo Viana <ricardviana1@gmail.com>
2026-02-15 10:51:10 -0300

    Incluindo arquivos para versionamento

---
+++ app.py
@@ -0,0 +1,33 @@
+# Importar biblioteca completa - padrão
+import io
+import unicodedata
+
+# Importar biblioteca completa - terceiro
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
+import shap
+import streamlit as st
+
+# Variavies 
+validar_shap = 'n'
+
+# CONFIGURAÇÃO DA PÁGINA
+st.set_page_config(
+    page_title="Predição de Risco de Obesidade", # Nome que vai aparecer na página do navegador
+    page_icon="🩺", # Icone da página
+    layout="centered" # Tipo do layout da pagina
+)
+
+# DEFINIÇÃO DE FUNÇÕES
+
+# Ordenar dados
+
+# Função princial
+def main():
+      print("Teste")
+      
+if __name__ == "__main__":
+    main()
